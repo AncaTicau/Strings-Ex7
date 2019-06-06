4585325463811aa780547d3b80662aa095393f92
MariusA <anca.ticau@gmail.com>
2019-06-06 23:08:20 +0300

    Exercise 7

---
+++ Strings-Ex7/Strings-Ex7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Strings_Ex7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string infoBlabla = RemoveNewline(@"       a1
+
+                                                        a2
+
+
+                                                a3
+
+                                              ");
+            Console.WriteLine(infoBlabla);
+        }
+
+        //Write a method that to remove a newline.
+
+        public static string RemoveNewline(string input)
+        {
+            string newLine = @"
+";
+            string changedString = input.Replace(newLine, "");
+
+            return changedString;
+        }
+    }
+}
